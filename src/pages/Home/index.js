8f6545efac535232f4186c3a884fc17e982d1059
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-31 18:14:36 -0300

    Inicia construção de listagem de viagens disponíveis obtidas da api e exibindo na Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { MdFlightTakeoff } from 'react-icons/md'
+
 import api from '../../services/api';
 
 export default function Home() {
@@ -19,9 +21,25 @@ export default function Home() {
 
   return (
     <div>
-      <h1>
-          Home
-      </h1>
-    </div>
+     <div className="box">
+       {trips.map(trip => (
+         <li key={trip.id}>
+           <img src={trip.image} alt={trip.title} />
+           <strong>{trip.title}</strong>
+           <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+
+           <button
+           type="button"
+           onClick={()=> {}}
+           >  
+             <div>
+               <MdFlightTakeoff size={16} color="#FFF" />
+             </div>
+             <span>SOLICITAR RESERVA</span> 
+           </button>
+         </li>
+       ))}
+     </div>
+   </div>
   );
 }
