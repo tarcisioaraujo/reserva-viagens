3adc5524180ec8decc6013d4482828085d1ac30d
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-31 19:59:38 -0300

    Reliza solucitação de reserva com redux e consome informação do reducer exibindo a mesma no contator de reservas no cabeçalho.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { MdFlightTakeoff } from 'react-icons/md'
+import { useDispatch } from 'react-redux';
 
 import api from '../../services/api';
 import './style.css';
 
 export default function Home() {
+  const dispatch = useDispatch(); 
   const [trips, setTrips] = useState([]);
 
   useEffect(() => {
@@ -20,6 +22,13 @@ export default function Home() {
 
   }, []);
 
+  function handleAdd(trip){
+    dispatch({
+      type: 'ADD_RESERVE',
+      trip
+    });
+  }
+
   return (
     <div>
      <div className="box">
@@ -31,7 +40,7 @@ export default function Home() {
 
            <button
            type="button"
-           onClick={()=> {}}
+           onClick={()=> handleAdd(trip)}
            >  
              <div>
                <MdFlightTakeoff size={16} color="#FFF" />
--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -1,3 +1,11 @@
-export default function reserve(){
-  return [];
+export default function reserve(state = [], action) {
+  
+  switch(action.type) {
+    case 'ADD_RESERVE':
+      return [ ...state,
+        action.trip 
+      ];
+    default:
+      return state;
+  }
 }
