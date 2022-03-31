61e5082b4b99b104f178ff0b756513e60bba7356
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-03-31 20:05:30 -0300

    Lista reservas dinamicamente obtidas pelo reducer.

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,26 +1,31 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 import { MdDelete } from 'react-icons/md';
 import './style.css';
 
 export default function Reservas() {
+  const reserves = useSelector(state => state.reserve);
+
   return (
     <div>
-    <h1 className="title">Voce solicitou 1 reservas</h1>
+    <h1 className="title">Voce solicitou {reserves.length} reservas</h1>
 
-    <div className="reservas">
-      <img
-      src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
-      alt="Maceio"
-      />
-      <strong>Viagem Maceio 7 Dias</strong>
-      <span>Quantidade: 2</span>
-      <button
-      type="button"
-      onClick={()=> {}}
-      >
+    {reserves.map(reserve => (
+      <div className="reservas" key={reserve.id}>
+        <img
+          src={reserve.image}
+          alt={reserve.title}
+        />
+        <strong>{reserve.title}</strong>
+        <span>Quantidade: 2</span>
+        <button
+          type="button"
+          onClick={()=> {}}
+        >
         <MdDelete size={20} color="#191919" />
       </button>
     </div>
+    ))}
 
     <footer>
       <button type="button">Solicitar Reservas</button>
