c32f90bbf10e24c47580b3ba278fb480f1d44bfa
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-04-03 18:07:39 -0300

    Refatora o saga apra evitar requisicões desnecessárias na api.

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,11 +1,28 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess } from './actions';
+import { call, put, all, takeLatest, select } from 'redux-saga/effects';
+import { addReserveSuccess, updateAmountReserve } from './actions';
 import api from '../../../services/api';
 
-function* addToReserve({ id }){
-    const response = yield call(api.get, `trips/${id}` );
+function* addToReserve({ id }) {
+    const tripExists = yield select(
+        state => state.reserve.find(trip => trip.id === id)
+    );
 
-    yield put(addReserveSuccess(response.data))
+    if (tripExists) {
+        const amount = tripExists.amount + 1;
+
+        yield put(updateAmountReserve(id, amount));
+    } else {
+        const response = yield call(api.get, `trips/${id}` );
+
+        const data = {
+            ...response.data,
+            amount: 1,
+        }
+
+        yield put(addReserveSuccess(data))
+    }
+
+    
 }
 
 export default all([
